c255758e57ee8c62c39db726e64ac218747b2bef
wangnan <wangnan7@jd.com>
2018-07-19 13:27:08 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -142,7 +142,7 @@ if (isProduction || isUpload) {
     if (isUpload) {
         webpackConfig.plugins = (webpackConfig.plugins || []).concat([
             new WebpackUploadPlugin({
-                host: "{{uploadHost}}",
+                host: "",//上传服务器地址
                 source: 'build',
                 serverDir: config.ftpServer,
                 target: config.ftpTarget
